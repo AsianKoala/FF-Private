9cb9b23b739605aedfbd70ef6e9f1b14bde2e3f1
Neil Mehra <neilmehra@outlook.com>
2021-11-02 20:19:41 -0400

    meep meep

--- TeamCode/src/main/java/robotuprising/ftc2021/auto/sim/MeepMeepTesting.kt
+++ TeamCode/src/main/java/robotuprising/ftc2021/auto/sim/MeepMeepTesting.kt
@@ -1,10 +1,14 @@
 package robotuprising.ftc2021.auto.sim
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.noahbres.meepmeep.MeepMeep
 import com.noahbres.meepmeep.MeepMeep.Background
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark
 import com.noahbres.meepmeep.roadrunner.DriveShim
+import robotuprising.ftc2021.auto.trajectorysequence.TrajectorySequenceBuilder
+import kotlin.math.PI
 
 object MeepMeepTesting {
     @JvmStatic
@@ -12,24 +16,54 @@ object MeepMeepTesting {
         // TODO: If you experience poor performance, enable this flag
         // System.setProperty("sun.java2d.opengl", "true");
 
-        // Declare a MeepMeep instance
-        // With a field size of 800 pixels
+        val deposit = Pose2d(8.0, -43.0, PI - PI / 4)
+        val intakeFwd = Pose2d(52.0, -52.0, PI - PI / 4)
+
         val mm = MeepMeep(800) // Set field image
-            .setBackground(Background.FIELD_ULTIMATE_GOAL_DARK) // Set theme
-            .setTheme(ColorSchemeRedDark()) // Background opacity from 0-1
-            .setBackgroundAlpha(1f) // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-            .setConstraints(60.0, 60.0, Math.toRadians(180.0), Math.toRadians(180.0), 15.0)
-            .followTrajectorySequence { drive: DriveShim ->
-                drive.trajectorySequenceBuilder(Pose2d(0.0, 0.0, 0.0))
-                    .forward(30.0)
-                    .turn(Math.toRadians(90.0))
-                    .forward(30.0)
-                    .turn(Math.toRadians(90.0))
-                    .forward(30.0)
-                    .turn(Math.toRadians(90.0))
-                    .forward(30.0)
-                    .turn(Math.toRadians(90.0))
-                    .build()
+                .setBackground(Background.FIELD_FREIGHT_FRENZY) // Set theme
+                .setTheme(ColorSchemeBlueDark()) // Background opacity from 0-1
+                .setBackgroundAlpha(1f) // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60.0, 60.0, Math.toRadians(180.0), Math.toRadians(180.0), 15.0)
+
+                .followTrajectorySequence { drive: DriveShim ->
+                    drive.trajectorySequenceBuilder(Pose2d(8.0, -60.0, PI / 2))
+                            .lineToLinearHeading(deposit)
+                            .waitSeconds(0.5)
+
+                            .turn(PI / 4)
+                            .setReversed(true)
+                            .splineToSplineHeading(intakeFwd, - PI / 4)
+                            .setReversed(false)
+                            .splineToSplineHeading(Pose2d(deposit.vec(), PI), PI)
+                            .turn(-PI / 4)
+                            .waitSeconds(0.5)
+
+                            .turn(PI / 4)
+                            .setReversed(true)
+                            .splineToSplineHeading(intakeFwd, - PI / 4)
+                            .setReversed(false)
+                            .splineToSplineHeading(Pose2d(deposit.vec(), PI), PI)
+                            .turn(-PI / 4)
+                            .waitSeconds(0.5)
+
+                            .turn(PI / 4)
+                            .setReversed(true)
+                            .splineToSplineHeading(intakeFwd, - PI / 4)
+                            .setReversed(false)
+                            .splineToSplineHeading(Pose2d(deposit.vec(), PI), PI)
+                            .turn(-PI / 4)
+                            .waitSeconds(0.5)
+
+                            .turn(PI / 4)
+                            .setReversed(true)
+                            .splineToSplineHeading(intakeFwd, - PI / 4)
+                            .setReversed(false)
+                            .splineToSplineHeading(Pose2d(deposit.vec(), PI), PI)
+                            .turn(-PI / 4)
+                            .waitSeconds(0.5)
+
+
+                            .build()
             }
             .start()
     }
--- TeamCode/src/test/java/SingtonTest.kt
+++
@@ -1,9 +0,0 @@
-import robotuprising.ftc2021.hardware.subsystems.Ayame
-
-object SingtonTest {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val m_homura = Ayame
-        m_homura.setFromMecanumPowers(MecanumPowers())
-    }
-}
