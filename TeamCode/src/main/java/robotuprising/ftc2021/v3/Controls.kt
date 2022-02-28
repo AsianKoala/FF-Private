5db0e1894828e5af30a9b24051932b206ef0f3a9
Neil Mehra <neilmehra@outlook.com>
2022-02-27 22:46:21 -0500

    cleaned controls

--- TeamCode/src/main/java/robotuprising/ftc2021/v3/Controls.kt
+++ TeamCode/src/main/java/robotuprising/ftc2021/v3/Controls.kt
@@ -9,37 +9,39 @@ import robotuprising.koawalib.subsystem.drive.MecanumDriveCommand
 
 class Controls (private val otonashi: Otonashi, private val driver: CommandGamepad, private val gunner: CommandGamepad) {
     fun bindControls() {
-        otonashi.drive.setDefaultCommand(MecanumDriveCommand(otonashi.drive, driver.leftStick, driver.rightStick))
+        val driveCommand = MecanumDriveCommand(otonashi.drive, driver.leftStick, driver.rightStick)
 
-        driver.rightTrigger.whenPressed(
-                IntakeCommands.IntakeSmartCommand(otonashi.intake)
-                        .andThen(
-                                IndexerCommands.Lock(otonashi.indexer)
-                                        .alongWith(OuttakeCommands.Transfer(otonashi.outtake))
-                        )
-        )
+        val intakeSequence = IntakeCommands.IntakeSmartCommand(otonashi.intake)
+                .andThen(
+                        IndexerCommands.Lock(otonashi.indexer)
+                                .alongWith(OuttakeCommands.Transfer(otonashi.outtake))
+                )
 
-        CommandScheduler.scheduleWatchdog({ driver.leftTrigger.isJustPressed && otonashi.hubState == HubState.ALLIANCE_HIGH && otonashi.intake.isMineralIn },
-                        TurretCommands.AllianceCommand(otonashi.turret)
+        val depositSequence = TurretCommands.AllianceCommand(otonashi.turret)
+                .alongWith(
+                        PitchCommands.AllianceHighCommand(otonashi.pitch),
+                        SlideCommands.AllianceHighCommand(otonashi.slides),
+                        WaitCommand(0.7).andThen(OuttakeCommands.Deposit(otonashi.outtake))
+                )
+
+                .continueIf(driver.rightBumper::isPressed)
+
+                .andThen(
+                        IndexerCommands.Index(otonashi.indexer).sleep(0.5),
+                        IndexerCommands.Home(otonashi.indexer),
+                        OuttakeCommands.Home(otonashi.outtake),
+                        TurretCommands.HomeCommand(otonashi.turret)
                                 .alongWith(
-                                        PitchCommands.AllianceHighCommand(otonashi.pitch),
-                                        SlideCommands.AllianceHighCommand(otonashi.slides),
-                                        WaitCommand(0.7).andThen(OuttakeCommands.Deposit(otonashi.outtake))
+                                        PitchCommands.HomeCommand(otonashi.pitch),
+                                        SlideCommands.HomeCommand(otonashi.slides)
                                 )
+                )
 
-                                .continueIf(driver.rightBumper::isPressed)
+        otonashi.drive.setDefaultCommand(driveCommand)
+        driver.rightTrigger.whenPressed(intakeSequence)
 
-                                .andThen(
-                                        IndexerCommands.Index(otonashi.indexer).sleep(0.5),
-                                        IndexerCommands.Home(otonashi.indexer),
-                                        OuttakeCommands.Home(otonashi.outtake),
-                                        TurretCommands.HomeCommand(otonashi.turret)
-                                                .alongWith(
-                                                        PitchCommands.HomeCommand(otonashi.pitch),
-                                                        SlideCommands.HomeCommand(otonashi.slides)
-                                                )
-                                )
-        )
+        CommandScheduler.scheduleWatchdog({
+            driver.leftTrigger.isJustPressed && otonashi.hubState == HubState.ALLIANCE_HIGH && otonashi.intake.isMineralIn }, depositSequence)
 
         gunner.dpadUp.whenPressed(InstantCommand(otonashi::incrementState))
         gunner.dpadDown.whenPressed(InstantCommand(otonashi::decrementState))
